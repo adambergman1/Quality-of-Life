41b3ed6686cb4ab68eb33b7c1a8209ffa1189933
Niklas Nilsson <nn222ia@student.lnu.se>
2019-12-19 17:57:15 +0100

    creating tables and inserted values

--- createTables.js
+++ createTables.js
@@ -1,23 +1,23 @@
 const pool = require('./config')
 
 const cities = `
-    CREATE TABLE cities (
-        city_id INT PRIMARY KEY AUTO_INCREMENT,
-        city VARCHAR(50) NOT NULL,
-        information TEXT,
-        country_id INT NOT NULL,
-        FOREIGN KEY (country_id) REFERENCES countries(country_id)
-    )
+  CREATE TABLE cities (
+    city_id INT PRIMARY KEY AUTO_INCREMENT,
+    city VARCHAR(50) NOT NULL,
+    information TEXT,
+    country_id INT NOT NULL,
+    FOREIGN KEY (country_id) REFERENCES countries(country_id)
+  )
 `
 const countries = `
-CREATE TABLE countries (
+  CREATE TABLE countries (
     country_id INT AUTO_INCREMENT,
     country VARCHAR(50) NOT NULL,
     PRIMARY KEY (country_id)
-)
+  )
 `
 const livingCost = `
-CREATE TABLE livingcost (
+  CREATE TABLE livingcost (
     cost_id INT PRIMARY KEY AUTO_INCREMENT,
     taxi FLOAT NOT NULL,
     cappuchino FLOAT NOT NULL,
@@ -27,22 +27,22 @@ CREATE TABLE livingcost (
     lunch FLOAT NOT NULL,
     city_id INT NOT NULL,
     FOREIGN KEY (city_id) REFERENCES cities(city_id)
-)
+  )
 `
 
 const runqueries = async () => {
 
-    try {
-        await pool.query('DROP TABLE IF EXISTS livingcost')
-        await pool.query('DROP TABLE IF EXISTS cities')
-        await pool.query('DROP TABLE IF EXISTS countries')
-        await pool.query(countries)
-        await pool.query(cities)
-        await pool.query(livingCost)
-        process.exit(0)
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    await pool.query('DROP TABLE IF EXISTS livingcost')
+    await pool.query('DROP TABLE IF EXISTS cities')
+    await pool.query('DROP TABLE IF EXISTS countries')
+    await pool.query(countries)
+    await pool.query(cities)
+    await pool.query(livingCost)
+    process.exit(0)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 runqueries()
--- fetchToDB.js
+++ fetchToDB.js
@@ -1,26 +1,92 @@
 const fetch = require('node-fetch')
 const pool = require('./config')
 
-const url = 'https://api.teleport.org/api/countries/'
+const countryUrl = 'https://api.teleport.org/api/countries/'
+const citiesUrl = 'https://api.teleport.org/api/urban_areas/'
 
-const getJson = async () => {
+const getJson = async (url) => {
+  try {
+
     const response = await fetch(url)
     const json = await response.json()
     return json
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-const insertToDB = async (json) => {
-    const res = await json
+const insertCountriesToDB = async (json) => {
+  const res = await json
+  const countries = res._links['country:items']
 
-    const countries = res._links['country:items']
+  countries.forEach(async country => {
+    pool.query(`INSERT INTO countries (country) VALUES (?)`, [country.name], (err) => {
+      if (err) console.log(err)
+    })
+  });
+}
 
-    countries.forEach(country => {
-        pool.query(`INSERT INTO countries (country) VALUES (?)`, [country.name], (err) => {
-            if (err) console.log(err)
+const insertCitiesToDB = async (json) => {
+  const res = await json
+  const cities = res._links['ua:item']
+
+  cities.forEach(async city => {
+    const cityJson = await getJson(city.href)
+    const tempArr = cityJson._links['ua:countries']
+    tempArr.forEach(async country => {
+      pool.query(`SELECT country_id FROM countries WHERE country="${country.name}"`, (err, result) => {
+        if (err) console.log(err)
+        pool.query(`INSERT INTO cities (city, country_id) VALUES(?,?)`, [city.name, result[0].country_id], (err, result) => {
+          if (err) console.log(err)
+          console.log(result)
         })
-    });
+      })
+    })
+  });
 }
 
-insertToDB(getJson())
-console.log(getJson())
+const insertCostLivingToDB = async () => {
+  const cities = await getCities()
+
+  cities[0].forEach(async res => {
+    const str = res.city.replace(/\s/g, '-').toLowerCase()
+    const city = str.replace(/[,.]/g, '')
+    const result = await getJson(`https://api.teleport.org/api/urban_areas/slug:${city}/details/`)
+    if (result.categories !== undefined) {
+      const costs = result.categories[3].data
+      let obj = {}
+      costs.forEach(cost => {
+        if (cost.id === 'COST-TAXI') {
+          obj.taxi = cost.currency_dollar_value
+        } else if (cost.id === 'COST-CAPPUCCINO') {
+          obj.cappuchino = cost.currency_dollar_value
+        } else if (cost.id === 'COST-IMPORT-BEER') {
+          obj.beer = cost.currency_dollar_value
+        } else if (cost.id === 'COST-FITNESS-CLUB') {
+          obj.gym_membership = cost.currency_dollar_value
+        } else if (cost.id === 'COST-PUBLIC-TRANSPORT') {
+          obj.monthly_public_transport = cost.currency_dollar_value
+        } else if (cost.id === 'COST-RESTAURANT-MEAL') {
+          obj.lunch = cost.currency_dollar_value
+        }
+
+        obj.city_id = res.city_id
+      })
+
+      pool.query(`INSERT INTO livingcost SET ?`, obj, (err) => {
+        if (err) console.log(err)
+      })
+    }
+  })
+}
+
+const getCities = async () => {
+  return pool.query('SELECT city, city_id FROM cities')
+
+}
+
+// insertCountriesToDB(getJson(countryUrl))
+// insertCitiesToDB(getJson(citiesUrl))
+insertCostLivingToDB()
+
 
