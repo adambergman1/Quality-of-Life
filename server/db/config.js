21728d115c5fbba8d28cdbf34b0d285e7406abae
adambrgmn <adam@wordpressutvecklare.se>
2019-12-20 22:38:08 +0100

    Adding socketPath to config

--- server/db/config.js
+++ server/db/config.js
@@ -5,7 +5,8 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PWD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  socketPath: process.env.DB_SOCKET
 }).promise()
 
 module.exports = pool
